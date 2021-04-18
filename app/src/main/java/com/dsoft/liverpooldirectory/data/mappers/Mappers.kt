48683af8a6e274668336f9d3f457d7c781a0b161
Deni Dzhamurzaev <denitobibito@gmail.com>
2021-04-18 20:01:49 +0300

    V1.1 *Minor fix

--- app/src/main/java/com/dsoft/liverpooldirectory/data/mappers/Mappers.kt
+++ app/src/main/java/com/dsoft/liverpooldirectory/data/mappers/Mappers.kt
@@ -19,26 +19,22 @@ fun VKApiJSON.toModel(): List<VKWall> {
                 errorCode = error.error_code ?: 0
             )
         )
+        return list
     } else {
-        for (item in response?.items!!) {
+        return response?.items?.map {
             val image =
-                item.attachments?.firstOrNull()?.photo?.sizes?.firstOrNull { size -> size.type == "r" }
-
-            list.add(
-                VKWall(
-                    text = item.text ?: "",
-                    image = image?.url ?: "",
-                    likesCount = item.likes?.count ?: 0,
-                    commentsCount = item.comments?.count ?: 0,
-                    viewCount = item.views?.count ?: 0,
-                    postId = item.id ?: 0,
-                    imageHeight = image?.height ?: 0,
-                    imageWidth = image?.width ?: 0,
-                    errorCode = 0
-                )
+                it.attachments?.firstOrNull()?.photo?.sizes?.firstOrNull { size -> size.type == "r" }
+            VKWall(
+                text = it.text ?: "",
+                image = image?.url ?: "",
+                likesCount = it.likes?.count ?: 0,
+                commentsCount = it.comments?.count ?: 0,
+                viewCount = it.views?.count ?: 0,
+                postId = it.id ?: 0,
+                imageHeight = image?.height ?: 0,
+                imageWidth = image?.width ?: 0,
+                errorCode = 0
             )
-        }
+        }!!.toList()
     }
-
-    return list.toList()
 }
